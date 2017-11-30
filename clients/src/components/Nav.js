c24c6deafa4aef40aec9df3334e08218c15149e2
N D <detroitmuseumofhealing@gmail.com>
2017-11-30 18:10:09 -0500

    hamburger

--- clients/src/components/Nav.js
+++ clients/src/components/Nav.js
@@ -6,16 +6,24 @@ import SearchForm from "./SearchForm";
 class Nav extends Component {
     render() {
         return (
+
             <Router>
                 <div className="mainNav">
+
                     <Link to="/Discover">Discover</Link>{' '}
                     <Link to="/SearchForm">Search</Link>
+
+                    <p className="hamburger ion-navicon-round"></p>
+
+
                     <div className="paths">
+
                         <Route path="/Discover" component={Discover} />
                         <Route path="/SearchForm" component={SearchForm} />
                     </div>
                 </div>
             </Router>
+
         );
     }
 }
