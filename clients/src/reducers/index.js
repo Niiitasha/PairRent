ee9bb2d5d0aeb54bd268ff40ffbca72c7bd7cbf9
Raevan Gregson <regregson@fullsail.edu>
2017-12-05 13:08:12 -0500

    Algo Fixed

--- clients/src/reducers/index.js
+++ clients/src/reducers/index.js
@@ -1,62 +1,57 @@
 const INITIAL_STATE = {
-    currentCardIndex: 0,
-    data: [],
-    isLoading: true,
-    matches: [],
-    matchCount: 0
+  currentCardIndex: 0,
+  data: [],
+  isLoading: true,
+  matches: [],
+  matchCount: 0
 };
 
-export default function (state = INITIAL_STATE, action) {
-    switch (action.type) {
-        case "REQUEST_PROFILES":
-            return Object.assign({}, state, {
-                isLoading: true
-            });
-        case "RECEIVE_PROFILES":
-            return Object.assign({}, state, {
-                data: action.data,
-                isLoading: false
-            });
-        case "MAKE_MATCHES":
-            var madeMatches = matchLogic(state, action.searchData);
-            return Object.assign({}, state, {
-                matches: madeMatches
-            });
-        case "NEXT":
-            return Object.assign({}, state, {
-                currentCardIndex: state.currentCardIndex + 1,
-            });
-        case "PREV":
-            return Object.assign({}, state, {
-                currentCardIndex: state.currentCardIndex - 1,
-            });
-        default:
-            return state;
-    }
+export default function(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case "REQUEST_PROFILES":
+      return Object.assign({}, state, {isLoading: true});
+    case "RECEIVE_PROFILES":
+      return Object.assign({}, state, {
+        data: action.data,
+        isLoading: false
+      });
+    case "MAKE_MATCHES":
+      var madeMatches = matchLogic(state, action.searchData);
+      return Object.assign({}, state, {matches: madeMatches});
+    case "NEXT":
+      return Object.assign({}, state, {
+        currentCardIndex: state.currentCardIndex + 1
+      });
+    case "PREV":
+      return Object.assign({}, state, {
+        currentCardIndex: state.currentCardIndex - 1
+      });
+    default:
+      return state;
+  }
 }
 
 function matchLogic(state, searchData) {
 
-    var matches = [];
+  var matches = [];
 
-    state.data.forEach(function (user) {
-        for (var userProperty in user) {
-            for (var searchProperty in searchData) {
-                if (userProperty === searchProperty) {
-                    if (user[userProperty] === searchData[searchProperty]) {
-                        searchData.matchedProperties.push(user[userProperty]);
-                        if (matches.includes(user)) {
-                            break;
-                        } else {
-                            matches.push(user);
-                        }
-                        state.matchCount = matches.length;
-                    } else {
-                        state.matchCount = matches.length;
-                    }
-                }
-            }
+  state.data.forEach(function(user) {
+    var userPropertyMatches = 0;
+    var specifiedMatches = 0;
+    for (var searchProperty in searchData) {
+      if (searchData[searchProperty] !== null) {
+        specifiedMatches++
+        if (user[searchProperty] === searchData[searchProperty]) {
+          userPropertyMatches++;
         }
-    });
-    return matches
+      }
+    }
+    if(userPropertyMatches>0){
+      match.push({user:user,percentage:userPropertyMatches%specifiedMatches});
+    }
+
+  });
+
+
+  return matches
 }
