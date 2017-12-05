454522bd6e7c90aed539847c2b72bd74471ae720
N D <detroitmuseumofhealing@gmail.com>
2017-12-05 11:08:05 -0500

    home page styling

--- clients/src/components/Home.js
+++ clients/src/components/Home.js
@@ -6,9 +6,9 @@ class Home extends Component {
   render() {
     return (
       <div className="homePage">
-      <h1 className="headerMain">PAIRRENT</h1>
-        <div className="imgColumn"><img src="https://pixabay.com/p-1889405/?no_redirect"></img><p>This is a fun app to use!</p></div>
-        <div className="imgColumn2"><p>I found a roommate and a best friend on this app!</p><img src="https://pixabay.com/p-2326419/?no_redirect"></img></div>
+      <h2 className="headerMain">PAIRRENT</h2>
+        <div className="imgContainer"><img className="homeImg" src="images/david.jpg"></img><p className="homeImg">This is such a fun app to use!</p>
+        <p className="homeImg">I found a roommate who is now my best friend!</p><img className="homeImg" src="images/tamara.jpg"></img></div>
       </div>
     );
   }
