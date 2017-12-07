f81b190bb759158d0abed48ef4823585fb50c69e
Raevan Gregson <regregson@fullsail.edu>
2017-12-07 09:02:08 -0500

    Carousel

--- clients/src/components/Carousel.js
+++ clients/src/components/Carousel.js
@@ -44,44 +44,43 @@ class Carousel extends React.Component {
             <div className="col-md-12">
               <React_Bootstrap_Carousel
                 animation={true}
-                slideshowSpeed={7000}
+                slideshowSpeed={3000}
                 leftIcon={leftIcon}
                 rightIcon={rightIcon}
                 onSelect={this.onSelect}
                 ref={r=>this.slider=r}
                 className="carousel-fade"
               >
-                <div style={{height:400}}>
+                <div style={{height:400}} className='images'>
+                      <img style={{width:"100%",height:"100%"}} src="https://www.spellingcity.com/images/landing/parents/parents_landing_mom_kids.jpg" />
+                      <div
+                        style={{backgroundColor: 'rgba(0, 0, 0, .6)',width:'100%',height:'100%',paddingTop:'150px',color:'white',fontSize:'250%'}}
+                        className="carousel-center">
+                          PairRent<br/> A place for single Parents looking to find other single parents as roommates.
+                        </div>
+                  </div>
+                <div style={{height:400}} className='images'>
                   <img
                     style={{width:"100%",height:"100%"}}
-                    src="https://www.w3schools.com/bootstrap/la.jpg"
+                    src="http://static-yourtango-com.imgix.net/sites/default/files/image_blog/single%20parent%20life.jpg?auto=compress,format&q=30&fit=crop&w=774&h=386"
                   />
-                  <div className="carousel-caption">
-                    Image
-                  </div>
+                  <div
+                    style={{backgroundColor: 'rgba(0, 0, 0, .6)',width:'100%',height:'100%',paddingTop:'150px',color:'white',fontSize:'250%'}}
+                    className="carousel-center">
+                      Safe and Secure<br/> PairRent runs backgrounds on all of its users.
+                    </div>
                 </div>
-                <div style={{height:500,width:"100%",backgroundColor:"aqua"}}>
-                  <video className="carousel-center" controls style={{width:"75%"}} height="250">
-                    <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4"/>
-                  </video>
+                <div style={{height:400}} className='images'>
+                  <img
+                    style={{width:"100%",height:"100%"}}
+                    src="http://childrensministry.com/wp-content/upload/children/media/2012/06/Single-Parent-Ministry-From-a-Dads-Point-of-View.png"
+                  />
+                  <div
+                    style={{backgroundColor: 'rgba(0, 0, 0, .6)',width:'100%',height:'100%',paddingTop:'225px',color:'white',fontSize:'250%'}}
+                    className="carousel-center">
+                      center Text
+                    </div>
                 </div>
-                <div style={{height:500,width:"100%",backgroundColor:"lightpink"}}>
-                  <div className="carousel-center">
-                    center Text
-                  </div>
-                  <div className="carousel-center">
-                    Text
-                  </div>
-                </div>
-                <div style={{height:500,width:"100%",backgroundColor:"lightblue"}}>
-                  <span>
-                    text
-                  </span>
-                  <div className="carousel-center">
-                    Text
-                  </div>
-                </div>
-
               </React_Bootstrap_Carousel>
             </div>
           </div>
